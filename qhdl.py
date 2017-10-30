3cbd4b72cb740acb1c968c332750e6fe4978e54d
jbaker10 <jbaker10@nd.edu>
2017-10-30 19:36:57 -0400

    updates to the qbit and qstate - now observes is children bits.
    ToDo: problem when setting the vector - needs modification

---
+++ qhdl.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+# sys.path.appen("PATH TO GATE SIMULATOR")
+
+def main():
+    with open(sys.argv[0]) as f:
+        parsed_file = 
+
+if __name__ == '__main__':
+    main()
