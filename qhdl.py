f3c9038a9e610cc4ba0d0ba03ac5c78ddcf0f261
Andrew Litteken <andrew.litteken@gmail.com>
2017-10-31 00:40:02 -0400

    beginning of a recursive descent parser

--- qhdl.py
+++ qhdl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import getopt
+import re
 # sys.path.appen("PATH TO GATE SIMULATOR")
 
 def usage():
@@ -43,6 +44,66 @@ def command_line_parse():
 	input_file = args[0]
 
 	return input_file, output_file	
+
+def read_to_white(f):
+	string = ''
+	while(f.peek(1)!=' '):
+		c = f.read(1)
+		string+=1
+	return string
+
+def read_to_non_white(f):
+	while(f.peek(1)==' '):
+		c = f.read(1)
+
+def parser(input_file):
+	f = open(input_file, 'w')
+	end = False
+	while(not end):
+		end = parse_D(f)
+
+def parse_D(f):
+	read_to_non_white(f)
+	function = f.peek(4)
+	if function == "gate":
+		read_to_white(f)
+		read_to_non_white(f)
+		return parse_F(f) and parse_T(f)
+	else:
+		return parse_F(f) and parse_R(f)
+
+def parse_F(f):
+	name = read_to_white(f)
+	match = re.search('[A-Z]([A-Z0-9]|\-[A-Z0-9])', name)
+	if match:
+		return True
+	else return False
+	
+def parse_T(f):
+	read_to_non_white(f)
+	function = read_to_white(f)
+	if function == "matrix":
+		if parse_I(f) and parse_L(f):
+			read_to_non_white()
+			if f.read(1) == '{':
+				
+			else:
+				return False;
+		else:
+			return False
+	elif function == "series":
+		return parse_L(f) 
+	else:
+		return False
+
+def parse_I(f):
+	read_to_non_white(f)
+	value = read_to_white(f)
+	match = re.search('[0-9]+', value)
+	if match:
+		return True
+	else:
+		return False	
 
 def main():
 	input_file, output_file = command_line_parse()
