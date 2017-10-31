c71496924f7eee7a77fc7c3f1859744d0bbc8cd3
Andrew Litteken <andrew.litteken@gmail.com>
2017-10-31 17:35:38 -0400

    Parser for correctness

--- qhdl.py
+++ qhdl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import time
 import getopt
 import re
 # sys.path.appen("PATH TO GATE SIMULATOR")
@@ -45,68 +46,94 @@ def command_line_parse():
 
 	return input_file, output_file	
 
-def read_to_white(f):
+def read_token(f):
 	string = ''
-	while(f.peek(1)!=' '):
+	c = f.read(1)
+	while c==' ' or c=='\n' or c=='\t':
 		c = f.read(1)
-		string+=1
-	return string
-
-def read_to_non_white(f):
-	while(f.peek(1)==' '):
+	if c==';':
+		string += c
+		return string.rstrip()
+	while c!=' ' and c!='\n' and c!='' and c!='\t' and c!=';':
+		string+=c
 		c = f.read(1)
+	if c==';':
+		f.seek(f.tell()-1)
+	return string.rstrip()
 
 def expect(pattern, f):
-	
+	string = read_token(f)
+	if string == pattern:
+		return True
+	else:
+		return False
 
 def parser(input_file):
-	f = open(input_file, 'w')
+	f = open(input_file, 'r+')
 	end = False
 	success = True
 	while(success and not end):
 		success, end = parse_D(f)
 
-def parse_D(f):
-	read_to_non_white(f)
-	if f.peek(1) == '':
-		return True, True
-	function = f.peek(4)
-	if function == "gate":
-		read_to_white(f)
-		read_to_non_white(f)
-		return parse_F(f) and parse_T(f) and expect(';', f), False
+	if success and end:
+		print("Parse Successful")
+		return True
 	else:
-		return parse_F(f) and parse_R(f) and expect(';', f), False
+		print("Parse Failed")
+		return False
+	
+
+def parse_D(f):
+	savepos = f.tell()
+	token = read_token(f)
+	if token == '':
+		return True, True
+	if token == "gate":
+		return parse_F(f) and parse_T(f), False
+	elif token == "measure":
+		return parse_R(f) and expect(';', f), False
+	elif token == "inverse":
+		return parse_F(f) and parse_F(f) and expect(';', f), False
+	else:
+		f.seek(savepos)
+		return parse_F(f) and parse_L(f) and expect(';', f), False
 
 def parse_F(f):
-	name = read_to_white(f)
-	match = re.search('[A-Z]([A-Z0-9]|\-[A-Z0-9])', name)
+	name = read_token(f)
+	match = re.search('[A-Z]([A-Z0-9]|\-[A-Z0-9])*', name)
 	if match:
 		return True
-	else return False
+	else:
+		return False
+
+def parse_R(f):
+	register = read_token(f)
+	match = re.search('[a-z][a-z0-9_]*', register)
+	if match:
+		return True
+	else:
+		return False
 	
 def parse_T(f):
-	read_to_non_white(f)
-	function = read_to_white(f)
+	function = read_token(f)
 	if function == "matrix":
 		i_value = parse_I(f)
-		l_value = parse_L(f)
+		r_value = parse_R(f)
 		between1 = expect('{', f)
 		m_value = parse_M(f)
 		between2 = expect('}', f)
-		return i_value and l_value and between1 and m_value and between2
+		return i_value and r_value and between1 and m_value and between2
 	elif function == "series":
 		l_value = parse_L(f)
 		between1 = expect('{', f)
 		k_value = parse_K(f)
 		between2 = expect('}', f)
-		return i_value and between1 and k_value and between2
+		return l_value and between1 and k_value and between2
 	else:
 		return False
 
 def parse_I(f):
-	read_to_non_white(f)
-	value = read_to_white(f)
+	value = read_token(f)
 	match = re.search('[0-9]+', value)
 	if match:
 		return True
@@ -116,14 +143,74 @@ def parse_I(f):
 def parse_L(f):
 	return parse_R(f) and parse_L_prime(f)
 
-def parse_L_prime(f)
-	if f.peek(1)==' ':
+def parse_L_prime(f):
+	savepos = f.tell()
+	string = read_token(f)	
+	f.seek(savepos)
+	if string==';' or string == '{':
 		return True
 	else:
 		return parse_R(f) and parse_L_prime(f)
 
+def parse_M(f):
+	return parse_J(f) and parse_M_prime(f)
+
+def parse_M_prime(f):
+	savepos = f.tell()
+	string = read_token(f)	
+	f.seek(savepos)
+	if string=='}':
+		return True
+	else:
+		return parse_J(f) and parse_M_prime(f)
+
+def parse_J(f):
+	return parse_E(f) and parse_J_prime(f)
+
+def parse_J_prime(f):
+	savepos = f.tell()
+	operation = read_token(f)
+	if operation == '+':
+		return parse_E(f) and parse_Im(f)
+	elif operation == '-':
+		return parse_E(f) and parse_Im(f)
+	else:
+		f.seek(savepos)
+		return True
+
+def parse_E(f):
+	savepos = f.tell()
+	token = read_token(f)
+	if token == '-':
+		return parse_I(f)
+	else:
+		f.seek(savepos)
+		return parse_I(f)
+
+def parse_Im(f):
+	token = read_token(f)
+	if token == 'j' or token =='i':
+		return True
+	else:
+		return False	
+
+def parse_K(f):
+	return parse_F(f) and parse_L(f) and expect(';', f) and parse_K_prime(f)
+
+def parse_K_prime(f):
+	savepos = f.tell()
+	token = read_token(f)
+	if token != '}':
+		f.seek(savepos)
+		return parse_F(f) and parse_L(f) and expect(';', f) and parse_K_prime(f)
+	else:
+		f.seek(savepos)
+		return True
+	
+
 def main():
 	input_file, output_file = command_line_parse()
+	parser(input_file)
 
 if __name__ == '__main__':
     main()
