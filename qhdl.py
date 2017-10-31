711f9da2d8f4582f361ea6d106bb185fca648c98
Andrew Litteken <andrew.litteken@gmail.com>
2017-10-30 22:34:17 -0400

    adding command line args

--- qhdl.py
+++ qhdl.py
@@ -1,10 +1,51 @@
 #!/usr/bin/env python3
-import sys
+import sys, getopt
 # sys.path.appen("PATH TO GATE SIMULATOR")
 
+def usage():
+	print('''
+	usage: qhdl.py [-h] [-o --output output_file] [--help]
+
+	-h --help                 : Show usage statement
+	-o --output=<output_file> : Use given output file
+	''')
+
+def command_line_parse():
+	output_file = "a.py"
+	input_file = ""
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], 'ho:', ["help", "output="])
+	except getopt.GetoptError as err:
+		print(err)
+		usage()
+		sys.exit(1)
+	
+	for o, a in opts:
+		if o in ("-o", "--output"):
+			output_file = a
+		elif o in ("-h", "--help"):
+			usage()
+			sys.exit()
+		else:
+			assert False
+			usage()
+			sys.exit(1)
+	
+	if len(args) > 1:
+		print("Too many agruments given")
+		usage()
+		sys.exit(2)
+	elif len(args) < 1:
+		print("Error: No program given")
+		usage()
+		sys.exit(2)
+	
+	input_file = args[0]
+
+	return input_file, output_file	
+
 def main():
-    with open(sys.argv[0]) as f:
-        parsed_file = 
+	input_file, output_file = command_line_parse()
 
 if __name__ == '__main__':
     main()
